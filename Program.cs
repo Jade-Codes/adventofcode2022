18dea9eb08527fbcbcea66418c3d1f870951443b
Jaydee123 <jade_wilson66@hotmail.co.uk>
2022-12-05 15:28:34 +0000

    Add reusuable methods

--- Program.cs
+++ Program.cs
@@ -4,10 +4,8 @@
     {
         static void Main(string[] args)
         {
-
-            var lines = File.ReadLines(@"C:\Users\jade_\adventofcode\data\challenge5.txt");
+            var lines = File.ReadLines("data\\challenge5.txt");
             Challenge5.Part2(lines);
-
         }
 
     }
--- src/Challenge5.cs
+++ src/Challenge5.cs
@@ -4,46 +4,14 @@ namespace Challenges
 {
     public class Challenge5
     {
-        public static void Part1(IEnumerable<string> lines)
+        public static void Part1(IEnumerable<string> lines, int rowIndex = 8, int columnIndex = 9)
         {
-            var linesArray = lines.ToArray();
-            var arrayMatrix = new string[9][];
-            var stackList = new List<Stack<string>>();
-            var actions = new List<int[]>();
-
-            for (var i = 0; i < 8; i++)
-            {
-                var charArrayMatrix = linesArray[i].Chunk(4).ToArray();
-                for (var j = 0; j < 9; j++)
-                {
-                    var currentString = string.Join("", charArrayMatrix[j]);
-
-                    if (!string.IsNullOrWhiteSpace(currentString))
-                    {
-                        if (arrayMatrix[j] is null)
-                        {
-                            arrayMatrix[j] = new string[8];
-                        }
-                        arrayMatrix[j][i] = currentString;
-                    }
-                }
-            }
-
-            for (var i = 0; i < arrayMatrix.Length; i++)
-            {
-                var list = arrayMatrix[i].Where(_ => !string.IsNullOrEmpty(_)).ToList();
-                list.Reverse();
-                stackList.Add(new Stack<string>(list));
-            }
-
-            for (var i = arrayMatrix.Length + 1; i < linesArray.Length; i++)
-            {
-                var numbers = Regex.Split(linesArray[i], @"\D+").Where(_ => !string.IsNullOrEmpty(_)).Select(Int32.Parse).ToList().ToArray();
-                actions.Add(numbers);
-            }
+            GetArrayMatrix(lines, rowIndex, columnIndex, out var arrayMatrix);
+            GetStackList(arrayMatrix, out var stackList);
+            GetActionsList(lines, arrayMatrix, out var actionList);
 
             var stackArray = stackList.ToArray();
-            foreach (var action in actions)
+            foreach (var action in actionList)
             {
                 var previousStack = stackArray[action[1] - 1];
                 var currentStack = stackArray[action[2] - 1];
@@ -65,46 +33,14 @@ namespace Challenges
             }
         }
 
-        public static void Part2(IEnumerable<string> lines)
+        public static void Part2(IEnumerable<string> lines, int rowIndex = 8, int columnIndex = 9)
         {
-            var linesArray = lines.ToArray();
-            var arrayMatrix = new string[9][];
-            var stackList = new List<Stack<string>>();
-            var actions = new List<int[]>();
-
-            for (var i = 0; i < 8; i++)
-            {
-                var charArrayMatrix = linesArray[i].Chunk(4).ToArray();
-                for (var j = 0; j < 9; j++)
-                {
-                    var currentString = string.Join("", charArrayMatrix[j]);
-
-                    if (!string.IsNullOrWhiteSpace(currentString))
-                    {
-                        if (arrayMatrix[j] is null)
-                        {
-                            arrayMatrix[j] = new string[8];
-                        }
-                        arrayMatrix[j][i] = currentString;
-                    }
-                }
-            }
-
-            for (var i = 0; i < arrayMatrix.Length; i++)
-            {
-                var list = arrayMatrix[i].Where(_ => !string.IsNullOrEmpty(_)).ToList();
-                list.Reverse();
-                stackList.Add(new Stack<string>(list));
-            }
-
-            for (var i = arrayMatrix.Length + 1; i < linesArray.Length; i++)
-            {
-                var numbers = Regex.Split(linesArray[i], @"\D+").Where(_ => !string.IsNullOrEmpty(_)).Select(Int32.Parse).ToList().ToArray();
-                actions.Add(numbers);
-            }
+            GetArrayMatrix(lines, rowIndex, columnIndex, out var arrayMatrix);
+            GetStackList(arrayMatrix, out var stackList);
+            GetActionsList(lines, arrayMatrix, out var actionList);
 
             var stackArray = stackList.ToArray();
-            foreach (var action in actions)
+            foreach (var action in actionList)
             {
                 var previousStack = stackArray[action[1] - 1];
                 var currentStack = stackArray[action[2] - 1];
@@ -135,5 +71,53 @@ namespace Challenges
             }
         }
 
+        private static void GetArrayMatrix(IEnumerable<string> lines, int rowIndex, int columnIndex, out string[][] arrayMatrix)
+        {
+            var linesArray = lines.ToArray();
+            arrayMatrix = new string[columnIndex][];
+
+            for (var i = 0; i < rowIndex; i++)
+            {
+                var charArrayMatrix = linesArray[i].Chunk(4).ToArray();
+                for (var j = 0; j < columnIndex; j++)
+                {
+                    var currentString = string.Join("", charArrayMatrix[j]);
+
+                    if (!string.IsNullOrWhiteSpace(currentString))
+                    {
+                        if (arrayMatrix[j] is null)
+                        {
+                            arrayMatrix[j] = new string[8];
+                        }
+                        arrayMatrix[j][i] = currentString;
+                    }
+                }
+            }
+        }
+
+        private static void GetStackList(string[][] arrayMatrix, out List<Stack<string>> stackList)
+        {
+            stackList = new List<Stack<string>>();
+
+            for (var i = 0; i < arrayMatrix.Length; i++)
+            {
+                var list = arrayMatrix[i].Where(_ => !string.IsNullOrEmpty(_)).ToList();
+                list.Reverse();
+                stackList.Add(new Stack<string>(list));
+            }
+        }
+
+        
+        private static void GetActionsList(IEnumerable<string> lines, string[][] arrayMatrix, out List<int[]> actionList)
+        {   
+            var linesArray = lines.ToArray();
+            actionList = new List<int[]>();
+            
+            for (var i = arrayMatrix.Length + 1; i < linesArray.Length; i++)
+            {
+                var numbers = Regex.Split(linesArray[i], @"\D+").Where(_ => !string.IsNullOrEmpty(_)).Select(Int32.Parse).ToList().ToArray();
+                actionList.Add(numbers);
+            }
+        }
     }
 }
